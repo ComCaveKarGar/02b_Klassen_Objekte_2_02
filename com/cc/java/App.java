43dbad5b10785057485acd2e9741466c3aa8bfb9
sven-pke <sven-perschke@gmx.de>
2023-05-24 10:16:02 +0200

    Klassendiagramm korrigiert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Grizabella", "white", 29, true);
+        Cat cat = new Cat("Grizabella", "white", 29);
 
         output(cat.getStringAttributes("#name"));
         output(cat.getStringAttributes("#color"));
@@ -15,7 +15,7 @@ public class App {
 
         output("-------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "grey", 35, false);
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
 
         output(cat2.getStringAttributes("#name"));
         output(cat2.getStringAttributes("#color"));
